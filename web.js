d86eeaea7c88be1ba7521dd642ec5a4669807aa6
Krishnaswamy90 <krishnachitra21@gmail.com>
2025-10-28 15:58:20 +0530

    initial

---
+++ web.js
@@ -0,0 +1,4 @@
+alert("More features Coming soon")
+alert("Suggesstions are welcome")
+alert("In rare case you will get your first response late in rare case only so dont worry")
+alert("Best part? it can remember old message (memory resets with session)")
