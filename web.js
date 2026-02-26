13d323d3e2b24e4067cc30c26686114f8fca1809
Krishnaswamy90 <krishnachitra21@gmail.com>
2026-02-26 13:26:54 +0530

    first commit

--- web.js
+++
@@ -1,4 +0,0 @@
-alert("More features Coming soon")
-alert("Suggesstions are welcome")
-alert("In rare case you will get your first response late in rare case only so dont worry")
-alert("Best part? it can remember old message (memory resets with session)")
